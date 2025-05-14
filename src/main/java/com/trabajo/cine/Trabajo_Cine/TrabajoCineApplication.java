a8119416b2eeed2994b7219ced8455bd5ecf8146
gianmdp03 <gcastorina234@gmail.com>
2025-05-14 17:19:30 -0300

    primer commit

---
+++ src/main/java/com/trabajo/cine/Trabajo_Cine/TrabajoCineApplication.java
@@ -0,0 +1,13 @@
+package com.trabajo.cine.Trabajo_Cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabajo/cine/Trabajo_Cine/TrabajoCineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.cine.Trabajo_Cine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
